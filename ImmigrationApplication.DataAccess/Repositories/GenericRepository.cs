337ec5af43347f797396a7d72e9323b8eb6d6f12
pandu aluvala <pandu.aluvala54@smartdatasystems.net>
2016-12-05 15:12:13 -0500

    updated

---
+++ ImmigrationApplication.DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Linq.Expressions;
+using ImmigrationApplication.Model;
+using System.Data.Objects;
+
+namespace ImmigrationApplication.DataAccess
+{
+    public class GenericRepository<TEntity> where TEntity : class
+    {
+        internal ImmiEntities context;
+        internal ObjectSet<TEntity> dbSet;
+
+        public GenericRepository(ImmiEntities context)
+        {
+            this.context = context;
+            dbSet = context.CreateObjectSet<TEntity>();
+        }
+
+        public GenericRepository()
+        {
+        }
+
+        public virtual IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "")
+        {
+            IQueryable<TEntity> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        public virtual void Add(TEntity entity)
+        {
+            ObjectStateManager objectStateManager = context.ObjectStateManager;
+            dbSet.AddObject(entity);
+            objectStateManager.ChangeObjectState(entity, System.Data.EntityState.Added);
+        }
+
+        public virtual void Delete(TEntity entityToDelete)
+        {
+            ObjectStateManager objectStateManager = context.ObjectStateManager;
+            dbSet.Attach(entityToDelete);            
+            dbSet.DeleteObject(entityToDelete);
+            objectStateManager.ChangeObjectState(entityToDelete, System.Data.EntityState.Deleted);
+        }
+
+        public virtual void Update(TEntity entityToUpdate)
+        {
+            ObjectStateManager objectStateManager = context.ObjectStateManager;
+
+            dbSet.Attach(entityToUpdate);
+            objectStateManager.ChangeObjectState(entityToUpdate, System.Data.EntityState.Modified);
+        }
+    }
+}
---
+++ ImmigrationApplication.WebAPI/Controllers/PersonController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ImmigrationApplication.DataAccess;
+using System.Web.Http;
+using ImmigrationApplication.Model;
+
+namespace ImmigrationApplication.WebAPI.Controllers
+{
+    public class PersonController : Controller
+    {
+        private UnitOfWork uow = null; 
+
+        public PersonController()
+        {
+            uow = new UnitOfWork();
+        }
+
+        public PersonController(UnitOfWork uow_)
+        {
+            this.uow = uow_;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ContentResult GetPersonName(int id)
+        {
+            ContentResult result = new ContentResult();
+            try
+            {
+                result.Content = uow.PersonRepository.GetById(id).FirstName.ToString();
+            }
+            catch (Exception e)
+            {
+                // TO Do Exception handling
+            }
+            return result;
+        }
+
+        public EmptyResult AddPerson()
+        {
+            try
+            {
+                uow.PersonRepository.Add(new Person() { PersonID = 2, FamilyName = "Test", FirstName = "Jim", Gender = "M" });
+                uow.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                // TO Do Exception handling
+            }
+            return new EmptyResult();
+        }
+
+        public EmptyResult DeletePerson()
+        {
+            try
+            {
+                uow.PersonRepository.Delete(new Person() { PersonID = 2, FamilyName = "Test", FirstName = "JimUpd", Gender = "M" });
+                uow.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                // TO Do Exception handling
+            }
+
+            return new EmptyResult();
+        }
+
+        public EmptyResult UpdatePerson()
+        {
+            try
+            {
+                uow.PersonRepository.Update(new Person() { PersonID = 2, FamilyName = "Test", FirstName = "JimUpd", Gender = "M" });
+                uow.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                // TO Do Exception handling
+            }
+            return new EmptyResult();
+        }
+    }
+}
